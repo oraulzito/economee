b168a07889f1a60d16662acdd9719bbeab50c0a0
Raul Lacerda <raul_lacerda@outlook.com>
2021-06-22 11:27:50 -0300

    Initial Commit - Done in 21/06/2021 - With Gabriel and Thiago

---
+++ economee/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for economee project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'economee.settings')
+
+application = get_wsgi_application()
