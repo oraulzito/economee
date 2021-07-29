50b62094284849c6efacca853ae84304c41fd829
Raul Lacerda <raul_lacerda@outlook.com>
2021-07-28 21:24:12 -0300

    New Feature - Start of the app dashboard layout.
    Starting the dashboard layout to show data, backend still to do.
    
    https://trello.com/c/iDKf6HZG/10-cria%C3%A7%C3%A3o-do-conte%C3%BAdo-do-layout-principal

--- economeeFrontend/src/app/dashboard/dashboard.module.ts
+++ economeeFrontend/src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 import { DashboardComponent } from './dashboard.component';
+import {HeaderComponent} from "./dashboard-desktop/header/header.component";
+import {ReleasesCardComponent} from "../components/releases-card/releases-card.component";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzGridModule} from "ng-zorro-antd/grid";
 
 
 @NgModule({
-  imports: [DashboardRoutingModule],
-  declarations: [DashboardComponent],
+  imports: [DashboardRoutingModule, NzCardModule, NzGridModule],
+  declarations: [DashboardComponent, HeaderComponent, ReleasesCardComponent],
   exports: [DashboardComponent]
 })
 export class DashboardModule { }
