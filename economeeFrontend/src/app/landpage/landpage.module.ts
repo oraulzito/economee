1b344fbc6e33288b9a73bdf61f45dd13949ff4e0
Thiago Goulart <thiaggoulart@gmail.com>
2021-08-04 22:33:37 -0300

    New Feature/Break change - starting create landpage
    Change css
    
    https://trello.com/c/7qiOlB65/7-cria%C3%A7%C3%A3o-do-conte%C3%BAdo-para-landpage

--- economeeFrontend/src/app/landpage/landpage.module.ts
+++ economeeFrontend/src/app/landpage/landpage.module.ts
@@ -2,15 +2,17 @@ import {NgModule} from '@angular/core';
 
 import { LandpageRoutingModule } from './landpage-routing.module';
 import { LandpageComponent } from './landpage.component';
-import {NzCardModule} from "ng-zorro-antd/card";
-import {NzTabsModule} from "ng-zorro-antd/tabs";
+import {NzCardModule} from 'ng-zorro-antd/card';
+import {NzTabsModule} from 'ng-zorro-antd/tabs';
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 
 @NgModule({
   imports: [
     LandpageRoutingModule,
     NzCardModule,
-    NzTabsModule
+    NzTabsModule,
+    NzButtonModule
   ],
   declarations: [
     LandpageComponent
