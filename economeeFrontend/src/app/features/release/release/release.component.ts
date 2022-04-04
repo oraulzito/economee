8edc86a6ea0492b0f730f5a740285103f0032246
Raul Lacerda <raul_lacerda@outlook.com>
2022-04-03 22:39:00 -0300

    chore: releases adjusments on FE

---
+++ economeeFrontend/src/app/features/release/release/release.component.ts
@@ -0,0 +1,108 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {BalanceQuery} from "../../../core/state/balance/balance.query";
+import {CardQuery} from "../../../core/state/card/card.query";
+import {InvoiceQuery} from "../../../core/state/invoice/invoice.query";
+import {ReleaseService} from "../../../core/state/release/release.service";
+import {ReleaseQuery} from "../../../core/state/release/release.query";
+import {CardService} from "../../../core/state/card/card.service";
+import {getEntityType} from "@datorama/akita";
+import {CardState} from "../../../core/state/card/card.store";
+import {Release} from "../../../core/state/release/release.model";
+import {Observable} from "rxjs";
+import {ReleaseState} from "../../../core/state/release/release.store";
+
+@Component({
+  selector: 'app-release',
+  templateUrl: './release.component.html',
+  styleUrls: ['./release.component.less']
+})
+export class ReleaseComponent implements OnInit {
+  @Input()
+  releaseType: number;
+
+  cards: getEntityType<CardState>[];
+  cardID: number;
+  cardName: string;
+
+  releaseItems: Observable<getEntityType<ReleaseState>[]>;
+  releaseEditItem: Release;
+
+  isEditModalVisible = false;
+  isCreationModalVisible = false;
+
+  listOptions = [{
+    id: 1,
+    name: 'Tudo',
+  }, {
+    id: 2,
+    name: 'Despesas',
+  }, {
+    id: 3,
+    name: 'Receitas',
+  }, {
+    id: 4,
+    name: 'Despesas pagas',
+  }, {
+    id: 5,
+    name: 'Despesas não pagas',
+  }, {
+    id: 6,
+    name: 'Receitas recebidas',
+  }, {
+    id: 7,
+    name: 'Receitas não recebidas',
+  },]
+
+  constructor(
+    private balanceQuery: BalanceQuery,
+    private cardQuery: CardQuery,
+    private invoiceQuery: InvoiceQuery,
+    private releaseService: ReleaseService,
+    private releaseQuery: ReleaseQuery,
+    private cardService: CardService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    if (this.releaseType === 0) {
+      this.cardQuery.allCards$.subscribe(r => this.cards = r);
+
+      this.cardQuery.activeCard$.subscribe(r => {
+        this.cardName = r.name;
+        this.cardID = r.id;
+      });
+    }
+
+    this.loadReleases(this.releaseType);
+  }
+
+  loadReleases(id) {
+    if (id === 0) {
+      this.invoiceQuery.selectActiveId().subscribe(
+        (invoice) => {
+          if (invoice)
+            this.releaseItems = this.releaseQuery.queryReleases(0);
+        });
+    } else {
+      this.releaseItems = this.releaseQuery.queryReleases(id);
+    }
+  }
+
+  createRelease() {
+    this.isCreationModalVisible = !this.isCreationModalVisible;
+  }
+
+  editRelease(release: Release) {
+    this.releaseEditItem = release;
+    this.isEditModalVisible = !this.isEditModalVisible;
+  }
+
+  changeActiveCard(id) {
+    this.cardService.setActiveCard(id);
+    this.releaseItems = this.releaseQuery.queryReleases(4);
+  }
+
+  changeActiveReleaseList(id) {
+    this.releaseItems = this.releaseQuery.queryReleases(id);
+  }
+}
